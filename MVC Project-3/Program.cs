ca607a14b99ba47a16181e5ebb2f1c1d262057c6
Aysa <bd7hwd97w@code.edu.az>
2024-05-28 18:12:03 +0400

    task

--- MVC Project-3/Program.cs
+++ MVC Project-3/Program.cs
@@ -5,26 +5,9 @@ namespace MVC_Project_3
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
-
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapGet("/", () => "Hello World!");
 
             app.Run();
         }
